9a0b8f842873d33393cb0c8e0fc3a2ea24ac34cd
Massimiliano Galli <massimiliano.galli@cern.ch>
2021-02-22 20:50:20 +0100

    Completely refactor installation

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from distutils.core import setup
 import setuptools
 from importlib import import_module
 
@@ -10,16 +9,22 @@ for pkg in ['ROOT', 'cppyy', 'cppyy_backend']:
         raise ImportError('{} not found. You need a full working installation of ROOT to install this package.\n' \
                 'For more info, see: https://root.cern/install/'.format(pkg))
 
-setup(
+def get_dependencies(env_yaml_file):
+    import yaml
+    with open(env_yaml_file, "r") as f:
+        environment = yaml.safe_load(f)
+    dependencies = []
+    for dep in environment["dependencies"]:
+        if dep != "root" and not dep.startswith("python"):
+            dependencies.append(dep)
+    return dependencies
+
+setuptools.setup(
     name="ultimate_plotter",
-    version="0.0.0",
     author="Massimiliano Galli",
     author_email="massimiliano.galli.95@gmail.com",
     description="HEP data processor and plotter with interfaces to multiple scikit-hep packages",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    packages=["ultimate_plotter"],
+    install_requires=get_dependencies("ultimate_plotter_env.yml"),
+    scripts = ["scripts/plot_ratio.py"]
 )
