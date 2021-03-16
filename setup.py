971020105f5459cff3cdc566708220da9e43a770
Massimiliano Galli <massimiliano.galli@cern.ch>
2021-03-16 08:29:19 +0100

    Remove ROOT as a dependency

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
 import setuptools
 from importlib import import_module
 
-# Check ROOT related requirements and raise error if not found
-for pkg in ['ROOT', 'cppyy', 'cppyy_backend']:
-    try:
-        import_module(pkg)
-    except ModuleNotFoundError:
-        raise ImportError('{} not found. You need a full working installation of ROOT to install this package.\n' \
-                'For more info, see: https://root.cern/install/'.format(pkg))
-
 def get_dependencies(env_yaml_file):
     import yaml
     with open(env_yaml_file, "r") as f:
         environment = yaml.safe_load(f)
     dependencies = []
     for dep in environment["dependencies"]:
-        if dep != "root" and not dep.startswith("python"):
+        if not dep.startswith("python"):
             dependencies.append(dep)
     return dependencies
 
@@ -26,5 +18,5 @@ setuptools.setup(
     description="HEP data processor and plotter with interfaces to multiple scikit-hep packages",
     packages=["ultimate_plotter"],
     install_requires=get_dependencies("ultimate_plotter_env.yml"),
-    scripts = ["scripts/plot_ratio.py"]
+    scripts = ["scripts/plot_ratio.py", "scripts/basic_plot.py"]
 )
